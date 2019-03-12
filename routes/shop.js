b6b7007c94c3a12a2a4f2dbd0cd493db83d531e4
diegoajv <diegojosevilla@outlook.com>
2019-03-11 18:03:27 -0600

    Feat[routes]: Add admint and shop routes

---
+++ routes/shop.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+// / => GET
+router.get('/', shopController.getIndex);
+
+// /products => GET
+router.get('/products', shopController.getProducts);
+
+// /cart => GET
+router.get('/cart', shopController.getCart);
+
+// /orders => GET
+router.get('/orders', shopController.getOrders);
+
+// /checkout => GET
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
